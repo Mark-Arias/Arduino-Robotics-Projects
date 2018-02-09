d3ee3bc0bed9387ccdfb8819a3d45411a8753124
Mark-Arias <36010364+Mark-Arias@users.noreply.github.com>
2018-02-08 22:15:21 -0800

    Add files via upload

---
+++ servoMove.ino
@@ -0,0 +1,22 @@
+//This program is more is used for more practice in controling servo's
+
+#include<Servo.h>
+Servo myServo;
+int delayTime = 1000; //motor is not able to turn faster than 375ms (.375)
+void setup() 
+{
+  myServo.attach(2);
+
+}
+
+void loop() 
+{
+  myServo.write(90);
+  delay(delayTime);
+  myServo.write(180);
+  delay(delayTime);
+  myServo.write(90);
+  delay(delayTime);
+  myServo.write(0);
+  delay(delayTime);
+}
